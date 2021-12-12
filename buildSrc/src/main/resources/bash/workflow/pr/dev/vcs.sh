1e2a25b00306f64d5ffe67e8e4973b66fd1de7d2
Stanley Wintergreen <kepocnhh@gmail.com>
2021-12-12 16:55:48 +0300

    wip | ci | pull request dev vcs fix...

--- buildSrc/src/main/resources/bash/workflow/pr/dev/vcs.sh
+++ buildSrc/src/main/resources/bash/workflow/pr/dev/vcs.sh
@@ -18,9 +18,9 @@ if test $CODE -ne 0; then
  exit 21
 fi
 
-PATH="/gh-pages"
-mkdir "${PATH}"
-git clone -q --depth=1 --branch=gh-pages https://$GITHUB_PAT@github.com/$GITHUB_OWNER/$GITHUB_REPO.git "${PATH}"
+REPOSITORY="/gh-pages"
+mkdir "${REPOSITORY}"
+git clone -q --depth=1 --branch=gh-pages https://$GITHUB_PAT@github.com/$GITHUB_OWNER/$GITHUB_REPO.git "${REPOSITORY}"
 WORKER_NAME="$(cat ${ASSEMBLY_PATH}/vcs/worker.json | jq -r .name)"
 if test -z "$WORKER_NAME"; then
  echo "Worker name is empty!"
@@ -40,15 +40,15 @@ if test -z "$GIT_COMMIT_SHA"; then
  exit 34
 fi
 
-mkdir -p "${PATH}/documentation/$VERSION"
-cp -r ${ASSEMBLY_PATH}/documentation/* "${PATH}/documentation/$VERSION"
+mkdir -p "${REPOSITORY}/documentation/$VERSION"
+cp -r ${ASSEMBLY_PATH}/documentation/* "${REPOSITORY}/documentation/$VERSION"
 
-git -C "${PATH}" config user.name "$WORKER_NAME" && \
- git -C "${PATH}" config user.email "$WORKER_EMAIL" && \
- git -C "${PATH}" add --all "${PATH}" && \
- git -C "${PATH}" commit -m \
+git -C "${REPOSITORY}" config user.name "$WORKER_NAME" && \
+ git -C "${REPOSITORY}" config user.email "$WORKER_EMAIL" && \
+ git -C "${REPOSITORY}" add --all "${REPOSITORY}" && \
+ git -C "${REPOSITORY}" commit -m \
   "GitHub build #$GITHUB_RUN_NUMBER | $WORKER_NAME added documentation for version $VERSION." && \
- git -C "${PATH}" push -f; CODE=$?
+ git -C "${REPOSITORY}" push -f; CODE=$?
 if test $CODE -ne 0; then
  echo "Git push failed!"
  exit 41
