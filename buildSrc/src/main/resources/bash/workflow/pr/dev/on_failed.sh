ec95cdd5afc6b72836805b55c1856f659c38ffbe
Stanley Wintergreen <kepocnhh@gmail.com>
2021-12-23 01:50:00 +0300

    wip | ci | on failed...

--- buildSrc/src/main/resources/bash/workflow/pr/dev/on_failed.sh
+++ buildSrc/src/main/resources/bash/workflow/pr/dev/on_failed.sh
@@ -5,28 +5,18 @@ echo "on failed pull request to dev start..."
 /bin/bash $RESOURCES_PATH/bash/workflow/vcs/close_pr.sh || exit 1 # todo
 
 /bin/bash $RESOURCES_PATH/bash/util/check_variables.sh \
- GITHUB_OWNER GITHUB_REPO GITHUB_RUN_NUMBER GITHUB_RUN_ID || exit 1 # todo
+ ASSEMBLY_PATH GITHUB_OWNER GITHUB_REPO GITHUB_RUN_NUMBER GITHUB_RUN_ID || exit 1 # todo
 
 RUN_URL="https://github.com/$GITHUB_OWNER/$GITHUB_REPO/actions/runs/$GITHUB_RUN_ID"
 BODY="Closed by GitHub build [#$GITHUB_RUN_NUMBER]($RUN_URL)."
 /bin/bash $RESOURCES_PATH/bash/workflow/vcs/post_comment.sh "$BODY" || exit 1 # todo
 
 AUTHOR_NAME="$(cat ${ASSEMBLY_PATH}/vcs/author.json | jq -r .name)"
-if test -z "$AUTHOR_NAME"; then
- echo "Author name is empty!"; exit 21
-fi
 AUTHOR_URL="$(cat ${ASSEMBLY_PATH}/vcs/author.json | jq -r .html_url)"
-if test -z "$AUTHOR_URL"; then
- echo "Author url is empty!"; exit 22
-fi
 WORKER_NAME="$(cat ${ASSEMBLY_PATH}/vcs/worker.json | jq -r .name)"
-if test -z "$WORKER_NAME"; then
- echo "Worker name is empty!"; exit 23
-fi
 WORKER_URL="$(cat ${ASSEMBLY_PATH}/vcs/worker.json | jq -r .html_url)"
-if test -z "$WORKER_URL"; then
- echo "Worker url is empty!"; exit 24
-fi
+for it in AUTHOR_NAME AUTHOR_URL WORKER_NAME WORKER_URL; do
+ if test -z "${!it}"; then echo "$it is empty!"; exit 21; fi; done
 
 REPO_URL=https://github.com/$GITHUB_OWNER/$GITHUB_REPO
 MESSAGE="GitHub build [#$GITHUB_RUN_NUMBER]($REPO_URL/actions/runs/$GITHUB_RUN_ID) failed!
--- buildSrc/src/main/resources/bash/workflow/telegram_send_message.sh
+++ buildSrc/src/main/resources/bash/workflow/telegram_send_message.sh
@@ -7,46 +7,30 @@ if test $# -ne 1; then
  exit 11
 fi
 
-if test -z "$telegram_bot_id"; then
- echo "Variable \"telegram_bot_id\" is empty!"
- exit 21
-fi
-
-if test -z "$telegram_bot_token"; then
- echo "Variable \"telegram_bot_token\" is empty!"
- exit 22
-fi
-
-if test -z "$telegram_chat_id"; then
- echo "Variable \"telegram_chat_id\" is empty!"
- exit 23
-fi
-
 MESSAGE=$1
 
 if test -z "$MESSAGE"; then
  echo "Message is empty!"
- exit 24
+ exit 12
 fi
 
-url="https://api.telegram.org/bot$telegram_bot_id:$telegram_bot_token/sendMessage"
+/bin/bash $RESOURCES_PATH/bash/util/check_variables.sh \
+ TELEGRAM_BOT_ID TELEGRAM_BOT_TOKEN TELEGRAM_CHAT_ID || exit 1 # todo
 
 MESSAGE=${MESSAGE//"#"/"%23"}
 MESSAGE=${MESSAGE//$'\n'/"%0A"}
 MESSAGE=${MESSAGE//$'\r'/""}
 
-CODE=0
+URL="https://api.telegram.org/bot$TELEGRAM_BOT_ID:$TELEGRAM_BOT_TOKEN/sendMessage"
 
-CODE=$(curl -w '%{http_code}\n' -G \
- -o /dev/null \
- -s $url \
- -d chat_id=$telegram_chat_id \
+CODE=$(curl -w %{http_code} -G -o /dev/null $URL \
+ -d chat_id=$TELEGRAM_CHAT_ID \
  -d text="$MESSAGE" \
  -d parse_mode=markdown)
 
 if test $CODE -ne 200; then
  echo "Request error with response code $CODE!"
- exit 41
+ exit 21
 fi
 
 echo "The message sent by telegram bot."
